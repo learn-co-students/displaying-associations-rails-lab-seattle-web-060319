d00fbf68c47a031cf4ddb413f217a09670c695e5
Michael J Maggs <michaelmaggs@gmail.com>
2019-07-02 14:36:07 -0700

    lab

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,17 +1,25 @@
+
+require 'pry'
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
+    @artists = Artist.all
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def new
+    @song = Song.new
+    @artists = Artist.all
   end
 
   def create
-    @song = Song.new(song_params)
-
+    @song = Song.new(song_params(:title, :artist_id))
     if @song.save
+
+      
       redirect_to @song
     else
       render :new
@@ -20,12 +28,13 @@ class SongsController < ApplicationController
 
   def edit
     @song = Song.find(params[:id])
+    @artists = Artist.all
   end
 
   def update
     @song = Song.find(params[:id])
 
-    @song.update(song_params)
+    @song.update(song_params(:title, :artist_id))
 
     if @song.save
       redirect_to @song
@@ -43,8 +52,8 @@ class SongsController < ApplicationController
 
   private
 
-  def song_params
-    params.require(:song).permit(:title)
+  def song_params(*args)
+    params.require(:song).permit(*args)
   end
 end
 
